90edfea5ee701ab9c7e79694c5454c4e4242311f
Adrian <Adriankzt05@gmail.com>
2023-12-11 15:44:12 +0800

    try commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.xinhui.mobfinalproject"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.xinhui.mobfinalproject"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
