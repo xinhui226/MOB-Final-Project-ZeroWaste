639ac26890656379c0799a010bcf9f328a1e0342
Xin Hui <xinhui@globalconnect@gmail.com>
2024-01-01 16:07:01 +0800

    added di inside the project

---
+++ app/src/main/java/com/xinhui/mobfinalproject/core/di/RepoModule.kt
@@ -0,0 +1,11 @@
+package com.xinhui.mobfinalproject.core.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class RepoModule {
+
+}
