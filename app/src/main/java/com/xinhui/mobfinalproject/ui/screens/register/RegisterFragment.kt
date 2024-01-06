4f4f94cfe2b369641e19bc7310951cb5bda96b08
Xin Hui <xinhui@globalconnect@gmail.com>
2024-01-06 16:38:35 +0800

    move validation to view model

--- app/src/main/java/com/xinhui/mobfinalproject/ui/screens/register/RegisterFragment.kt
+++ app/src/main/java/com/xinhui/mobfinalproject/ui/screens/register/RegisterFragment.kt
@@ -1,17 +1,12 @@
 package com.xinhui.mobfinalproject.ui.screens.register
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.google.android.material.snackbar.Snackbar
-import com.xinhui.mobfinalproject.R
 import com.xinhui.mobfinalproject.databinding.FragmentRegisterBinding
 import com.xinhui.mobfinalproject.ui.screens.base.BaseFragment
-import com.xinhui.mobfinalproject.ui.screens.base.viewModel.BaseViewModel
 import com.xinhui.mobfinalproject.ui.screens.register.viewModel.RegisterViewModelImpl
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -23,7 +18,7 @@ class RegisterFragment : BaseFragment<FragmentRegisterBinding>() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentRegisterBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -37,14 +32,7 @@ class RegisterFragment : BaseFragment<FragmentRegisterBinding>() {
                 val email = etEmail.text.toString()
                 val pass = etPass.text.toString()
                 val conPass = etConfirmPass.text.toString()
-
-                if (name.isEmpty() || email.isEmpty() || pass.isEmpty() || conPass.isEmpty()) {
-                    Snackbar.make(view, "Please fill in all fields", Snackbar.LENGTH_LONG).show()
-                } else if (pass != conPass) {
-                    Snackbar.make(view, "Password and Confirm Password is not the same", Snackbar.LENGTH_LONG).show()
-                } else {
-                    viewModel.register(name, email, pass, conPass)
-                }
+                viewModel.register(name, email, pass, conPass)
             }
 
             tvLoginNow.setOnClickListener {
